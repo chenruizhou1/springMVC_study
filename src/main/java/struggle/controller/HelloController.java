fd37b4122497976537f16062ccfbf063462c28fd
chenruizhou <1259522407@qq.com>
2021-09-01 18:06:16 +0800

    2021/9/1 C

--- src/main/java/struggle/controller/HelloController.java
+++ src/main/java/struggle/controller/HelloController.java
@@ -1,20 +1,21 @@
 package struggle.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.*;
 
 /**
  * @author chenruizhou
  * @date 2021/8/20
  */
-@Controller
+@RestController
 public class HelloController {
-
-    @RequestMapping("/")
-    public String index() {
-        return "index";
-    }
 
     @RequestMapping("/target")
     public String toTarget() {
@@ -28,5 +29,28 @@ public class HelloController {
         mav.setViewName("success");
         return mav;
     }
+
+    @RequestMapping(value = "/testPutRequestMethod", method = RequestMethod.PUT)
+    public ModelAndView testPutRequestMethod(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("testAttributeName1", username + "-" + password);
+        mv.addObject("testAttributeName2", "测试PUT请求方法成功！");
+        mv.setViewName("success");
+        return mv;
+    }
+
+    @RequestMapping ("/testResponseEntity")
+    public ResponseEntity<Map<String, Object>> testResponseEntity() {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        Map<String, Object> map = new HashMap<>(16);
+        map.put("UUID", UUID.randomUUID().toString());
+        map.put("message", "测试ResponseEntity成功！");
+        map.put("data", list);
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
 
 }
