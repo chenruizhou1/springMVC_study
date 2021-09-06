af995a37c45bce865ea8832099ef5b9e9febdb6d
chenruizhou <1259522407@qq.com>
2021-09-06 18:12:23 +0800

    2021/9/6 C

---
+++ src/main/java/struggle/Interceptor/MyFirstInterceptor.java
@@ -0,0 +1,33 @@
+package struggle.Interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author chenruizhou
+ * @date 2021/9/6
+ */
+@Component
+public class MyFirstInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("MyFirstInterceptor->preHandle");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("MyFirstInterceptor->postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("MyFirstInterceptor->afterCompletion");
+    }
+
+}
